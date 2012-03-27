586186cf48319a367545231850741f1b184f2429
object <vagif@online.no>
2012-03-27 15:44:48 +0200

    Added CommonAssemblyInfo.

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+using System.Resources;
+
+//
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("Vagif Abilov")]
+[assembly: AssemblyProduct("MongOData")]
+[assembly: AssemblyCopyright("Copyright (c) 2012 Vagif Abilov")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
--- Mongo.DataService/Properties/AssemblyInfo.cs
+++ Mongo.DataService/Properties/AssemblyInfo.cs
@@ -7,12 +7,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Mongo.DataService")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Vagif Abilov")]
-[assembly: AssemblyProduct("Mongo.DataService")]
-[assembly: AssemblyCopyright("Copyright © Vagif Abilov 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,16 +15,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1f5c3da5-cfba-4b03-a80e-73bbcacd9e24")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]

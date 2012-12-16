637279e3a746cfce6e10f076d4ce928372b29b49
Vagif Abilov <vagif@online.no>
2012-12-16 18:34:13 +0100

    Created samples projects.

---
+++ Samples/CreateSampleData/Entities.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CreateSampleData
+{
+    public class Quantity
+    {
+        public double Value { get; set; }
+        public string Units { get; set; }
+    }
+
+    public class Supplier
+    {
+        public string Name { get; set; }
+        public Address[] Addresses { get; set; }
+    }
+
+    public enum AddressType
+    {
+        Postal,
+        Street,
+    }
+
+    public class Address
+    {
+        public AddressType Type { get; set; }
+        public string[] Lines { get; set; }
+    }
+
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public DateTime? DiscontinueDate { get; set; }
+        public int Rating { get; set; }
+        public Quantity Quantity { get; set; }
+        public Category Category { get; set; }
+        public Supplier Supplier { get; set; }
+    }
+
+    public class Category
+    {
+        public Category()
+        {
+            this.Products = new List<Product>();
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
---
+++ Samples/SampleWcfDataServiceClient/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Services.Client;
+using System.Linq;
+using System.Text;
+
+namespace SampleWcfDataServiceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Connecting to MongoDB sample OData service...");
+                var context = new ServiceReference.MongoContext(new Uri("http://localhost:50336/MongoDataService.svc/"));
+
+                Console.WriteLine("Retrieving categories...");
+                Console.WriteLine();
+                foreach (var category in context.Categories)
+                {
+                    Console.WriteLine("Category: ID=[{0}], Name=[{1}]",
+                        category.ID,
+                        category.Name);
+                }
+                Console.WriteLine();
+
+                Console.WriteLine("Retrieving products...");
+                Console.WriteLine();
+                foreach (var product in context.Products)
+                {
+                    Console.WriteLine("Product: ID=[{0}], Name=[{1}], Category=[{2}], Quantity=[{3} {4}], ReleaseDate=[{5}], DiscontinueDate=[{6}]",
+                        product.ID,
+                        product.Name,
+                        product.Category.Name,
+                        product.Quantity.Value,
+                        product.Quantity.Units,
+                        product.ReleaseDate,
+                        product.DiscontinueDate);
+                }
+            }
+            catch (Exception exception)
+            {
+                if (exception is DataServiceClientException || exception.InnerException is DataServiceClientException)
+                {
+                    Console.WriteLine("The application was unable to process OData service response. This program doesn't support OData v.3 protocol. Check service metadata, in case its DataServiceVersion is set to 3, its data may not be consumed by this program.");
+                }
+                else
+                {
+                    Console.WriteLine("Error: {0}", exception.Message);
+                }
+            }
+        }
+    }
+}
